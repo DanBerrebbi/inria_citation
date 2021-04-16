41226aa2a249ff7b6c12eb7a424a165ed6387768
Dan Berrebbi <danberrebbi@macbook-pro-de-dan.home>
2021-04-17 00:35:29 +0200

    vraie jointure

--- jointure_data.sh
+++ jointure_data.sh
@@ -1,7 +1,7 @@
 
 
 for data in train dev test ; do
-  cat /Users/danberrebbi/Documents/inria_phase_2/acl-arc/$data.jsonl > /Users/danberrebbi/Documents/inria_phase_2/acl-arc/full_dataset.jsonl
+  cat /Users/danberrebbi/Documents/inria_phase_2/acl-arc/$data.jsonl >> /Users/danberrebbi/Documents/inria_phase_2/acl-arc/full_dataset.jsonl
   done
 
   echo jointure terminee
